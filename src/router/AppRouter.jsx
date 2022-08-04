903697df082d76e5bbaa0ae167f33c51dc1a53c5
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-04 15:32:39 +0200

    add index.js barrel file to export componenets

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { LoginPage } from '../auth/pages/LoginPage';
-import { MarvelPage } from '../heroes/pages/MarvelPage';
-import { DcPage } from '../heroes/pages/DcPage';
+
+import { DcPage, MarvelPage } from '../heroes';
+import { LoginPage } from '../auth';
 import { Navbar } from '../ui';
 
 export const AppRouter = () => {
