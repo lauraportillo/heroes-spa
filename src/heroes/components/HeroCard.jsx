cab3d8393e559196ff8dc58f08d9443b62cacdfb
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-06 15:03:08 +0200

    update

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,5 +1,13 @@
 import { Link } from 'react-router-dom';
 
+
+// CREAMOS UN PEQUEÑO COMPONENTE:
+const CharactersByHero = ({ alter_ego, characters }) => {
+  if (alter_ego === characters) return <></>;
+
+  return <p>{characters}</p>;
+};
+
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
   const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
@@ -16,7 +24,7 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
               <h5 className="card-title">{superhero}</h5>
               <p className="card-text">{alter_ego}</p>
 
-              {alter_ego !== characters && <p>{characters}</p>}
+              <CharactersByHero characters={characters} alter_ego={alter_ego} />
 
               <p className="card-text">
                 <small className="text-muted">{first_appearance}</small>
