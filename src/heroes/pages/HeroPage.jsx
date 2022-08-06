363f340321f63cd168d539724592344f62d6ffaf
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-06 17:15:34 +0200

    add styles to hero page detail

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,18 +1,48 @@
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useParams, useNavigate } from 'react-router-dom';
 import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
   const { id } = useParams();
-
+  const navigate = useNavigate();
   const hero = getHeroById(id);
+  const heroImageUrl = `/assets/heroes/${id}.jpg`;
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   if (!hero) {
     return <Navigate to="/marvel" />;
   }
 
   return (
-    <>
-      <h1>{hero.superhero}</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={heroImageUrl} alt={hero.superhero} className="img-thumbnail" />
+      </div>
+
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance:</b> {hero.first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3"> Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button onClick={onNavigateBack} className="btn btn-outline-primary">
+          Back
+        </button>
+      </div>
+    </div>
   );
 };
