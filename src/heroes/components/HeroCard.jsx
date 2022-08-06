ca101652ee216c72a9fe098da0e0a248a3895457
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-06 14:35:33 +0200

    add info to card component

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -8,6 +8,14 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
           <div className="col-4">
             <img src={heroImageUrl} className="card-img" alt={superhero} />
           </div>
+
+          <div className="col-8">
+            <div className="card-body">
+              <h5 className="card-title">{superhero}</h5>
+              <p className="card-text">{alter_ego}</p>
+              <p>{characters}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
