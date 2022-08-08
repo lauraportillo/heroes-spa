cbf1eda6ef0d4c156557f90b2e63e792959dd71c
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-08 15:56:15 +0200

    add conditionals to show mesagges

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -12,6 +12,9 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse(location.search);
   const heroes = getHeroesByName(q);
 
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange } = useForm({
     searchText: q,
   });
@@ -53,8 +56,17 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}
+          >
+            Search a hero
+          </div>
+
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}
+          >
             No hero with <b>{q}</b>
           </div>
 
