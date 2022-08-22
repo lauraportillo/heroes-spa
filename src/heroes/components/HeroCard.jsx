88ea2010ce3de2b3df702040156407ba5778e88e
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-22 16:19:29 +0200

    run build

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -8,15 +8,15 @@ const CharactersByHero = ({ alter_ego, characters }) => {
   return <p>{characters}</p>;
 };
 
-export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
-  const heroImageUrl = `/assets/heroes/${id}.jpg`;
+export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters, image }) => {
+  // const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
   return (
     <div className="col animate__animated animate__fadeIn">
       <div className="card">
         <div className="row no-gutters">
           <div className="col-4">
-            <img src={heroImageUrl} className="card-img" alt={superhero} />
+            <img src={image} className="card-img" alt={superhero} />
           </div>
 
           <div className="col-8">
