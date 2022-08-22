c80833814369364a8c0dffdfabaedae4a8405821
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-22 17:10:49 +0200

    delete folder

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -16,7 +16,7 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
       <div className="card">
         <div className="row no-gutters">
           <div className="col-4">
-            <img src={`./assets/heroes/${id}.jpg`} className="card-img" alt={superhero} />
+            <img src={`/assets/heroes/${id}.jpg`} className="card-img" alt={superhero} />
           </div>
 
           <div className="col-8">
