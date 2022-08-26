e275a031ae91fe18f8ffee59b6b096d3bb2f0f8a
Laura Portillo <lauraportillo.ark@gmail.com>
2022-08-26 16:55:23 +0200

    return to first url

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -9,7 +9,7 @@ const CharactersByHero = ({ alter_ego, characters }) => {
 };
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
-  const heroImageUrl = `/heroes-spa/public/assets/heroes/${id}.jpg`;
+  const heroImageUrl = `/assets/heroes/${id}.jpg`;
 
   return (
     <div className="col animate__animated animate__fadeIn">
